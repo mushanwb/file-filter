3c8097c3256624f79f9cce5b6c2f7c566af80f88
maxiaoda <631677979@qq.com>
2020-02-17 10:31:46 +0800

    Java面向对象初步练习：实现一个自定义的文件过滤器

--- src/main/java/com/github/hcsp/polymorphism/FileFilter.java
+++ src/main/java/com/github/hcsp/polymorphism/FileFilter.java
@@ -38,25 +38,4 @@ public class FileFilter {
         });
         return names;
     }
-
-//    static class FileFilterVisitor extends SimpleFileVisitor<Path> {
-//        private String extension;
-//        private List<String> filteredNames = new ArrayList<>();
-//
-//        public FileFilterVisitor(String extension) {
-//            this.extension = extension;
-//        }
-//
-//        public List<String> getFilteredNames() {
-//            return filteredNames;
-//        }
-//
-//        @Override
-//        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-//            if (file.getFileName().toString().endsWith(extension)) {
-//                filteredNames.add(file.getFileName().toString());
-//            }
-//            return FileVisitResult.CONTINUE;
-//        }
-//    }
 }
